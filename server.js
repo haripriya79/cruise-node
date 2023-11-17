f3bef4956430cfbef1974a539d3868a85c9afa09
Yesh-Chala <chalayeshwanth@gmail.com>
2023-11-17 00:28:48 -0700

    commiting all

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+app.get('/api', (req, res) => {
+    const jsonData = require('./public/data.json');
+    res.json(jsonData);
+  });
