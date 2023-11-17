928f2e8133dc0e6780dc1dc2509b388701b344fd
Yesh-Chala <chalayeshwanth@gmail.com>
2023-11-17 01:44:16 -0700

    2

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const path = require('path');
 
 const app = express();
+const router = express.Router();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
@@ -16,3 +18,16 @@ app.get('/api', (req, res) => {
     const jsonData = require('./public/data.json');
     res.json(jsonData);
   });
+
+  
+  router.get('/', (req, res) => {
+    const jsonData = require('./public/data.json');
+    res.json(jsonData);
+  });
+  
+  // Serve static files from the 'public' directory
+  app.use('/api', router);
+  
+  
+  module.exports.handler = serverless(app);
+  
